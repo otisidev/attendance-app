49d30607a509446a3be09fb06b0dc96ea532f2e5
Otis Chiemezie <otisidevelop@gmail.com>
2020-01-24 19:47:20 +0100

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,24 @@
+const { connect } = require("../context/icontext.service");
+const { ApolloServer } = require("apollo-server");
+
+//  ApolloServer instance
+const server = new ApolloServer({
+	modules: [require("./modules/department")],
+	introspection: true
+});
+
+// init database connection
+connect()
+	.then(
+		status =>
+			status === true &&
+			server
+				.listen(4900)
+				.then(
+					({ url, subscriptionsUrl }) =>
+						console.log(`Runing @ >_ ${url}`) ||
+						console.log("PubSub Server @ >_ " + subscriptionsUrl)
+				)
+				.catch(e => console.log("SERVER ERROR: ", e.message))
+	)
+	.catch(err => console.log("CONNECTION ERROR: ", err.message));
