33d232cee2e64faf65b1a6fe6ff08ddbad030888
Otis Chiemezie <otisidevelop@gmail.com>
2020-01-24 23:26:52 +0100

    Create session.model.js

---
+++ src/models/session.model.js
@@ -0,0 +1,28 @@
+const { model, Schema } = require("mongoose");
+
+const SessionSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true
+		},
+		active: {
+			type: Boolean,
+			default: false
+		},
+		removed: {
+			type: Boolean,
+			default: false
+		}
+	},
+	{ timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
+SessionSchema.virtual("id").get(function() {
+	return this._id;
+});
+
+SessionSchema.set("toJSON", { virtual: true });
+// public member
+exports.SessionModel = model("Session", SessionSchema);
