d5674e961f8408cf27f724bdec93de97988f2799
Otis Chiemezie <otisidevelop@gmail.com>
2020-01-25 00:44:27 +0100

    Create departmental-course.model.js

---
+++ src/models/departmental-course.model.js
@@ -0,0 +1,52 @@
+const { model, Schema } = require("mongoose");
+
+const DepartmentalCourseSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		code: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		level: {
+			type: Number,
+			required: true
+		},
+		creditUnit: {
+			type: Number,
+			required: true
+		},
+		department: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			trim: true,
+			ref: "Department"
+		},
+		lecturers: [
+			{
+				type: Schema.Types.ObjectId,
+				trim: true,
+				ref: "Lecturer"
+			}
+		],
+		removed: {
+			type: Boolean,
+			default: false
+		}
+	},
+	{ timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
+DepartmentalCourseSchema.virtual("id").get(function() {
+	return this._id;
+});
+
+DepartmentalCourseSchema.set("toJSON", { virtual: true });
+// public member
+exports.DepartmentalCourseModel = model(
+	"DepartmentalCourse",
+	DepartmentalCourseSchema
+);
