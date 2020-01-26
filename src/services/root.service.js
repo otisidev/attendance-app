139bd86f063b8a53cf571565e69d2e5c7e2d5639
Otis Chiemezie <otisidevelop@gmail.com>
2020-01-26 21:59:22 +0100

    Update root.service.js

--- src/services/root.service.js
+++ src/services/root.service.js
@@ -3,15 +3,26 @@ const { DepartmentService } = require("../services/department.service");
 const { SessionService } = require("../services/session.service");
 const { MaximumCreditService } = require("../services/maximum-credit.service");
 const { LogService } = require("../services/log.service");
+const { UserService } = require("../services/user.service");
+const { CoreService } = require("../../context/core.lib");
 
 // service instance
 const _dService = new DepartmentService();
 const _sService = new SessionService();
 const _mService = new MaximumCreditService();
 const _lService = new LogService();
+const _uService = new UserService();
+const coreService = new CoreService();
 
 // public instance
 module.exports = {
-	services: { _dService, _sService, _mService, _lService },
+	services: {
+		_dService,
+		_sService,
+		_mService,
+		_lService,
+		coreService,
+		_uService
+	},
 	helpers: {}
 };
