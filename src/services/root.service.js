afe02a5fec4b03ccbd1d3eb65be6e5e907d0cca0
Otis Chiemezie <otisidevelop@gmail.com>
2020-01-26 16:57:08 +0100

    Update root.service.js

--- src/services/root.service.js
+++ src/services/root.service.js
@@ -1,13 +1,15 @@
 // Import service class
 const { DepartmentService } = require("../services/department.service");
 const { SessionService } = require("../services/session.service");
+const { MaximumCreditService } = require("../services/maximum-credit.service");
 
 // service instance
 const _dService = new DepartmentService();
 const _sService = new SessionService();
+const _mService = new MaximumCreditService();
 
 // public instance
 module.exports = {
-	services: { _dService, _sService },
+	services: { _dService, _sService, _mService },
 	helpers: {}
 };
