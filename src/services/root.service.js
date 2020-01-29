222d7800928537ce4ac8fc8feb8c76996d15babe
Otis Chiemezie <otisidevelop@gmail.com>
2020-01-29 05:26:29 +0100

    Update root.service.js

--- src/services/root.service.js
+++ src/services/root.service.js
@@ -4,6 +4,10 @@ const { SessionService } = require("../services/session.service");
 const { MaximumCreditService } = require("../services/maximum-credit.service");
 const { LogService } = require("../services/log.service");
 const { UserService } = require("../services/user.service");
+const { LecturerService } = require("../services/lecturer.service");
+const {
+	DepartmentalCourseService
+} = require("../services/departmental-course.service");
 const { CoreService } = require("../../context/core.lib");
 
 // service instance
@@ -12,6 +16,8 @@ const _sService = new SessionService();
 const _mService = new MaximumCreditService();
 const _lService = new LogService();
 const _uService = new UserService();
+const _dcService = new DepartmentalCourseService();
+const _lecService = new LecturerService();
 const coreService = new CoreService();
 
 // public instance
@@ -22,7 +28,9 @@ module.exports = {
 		_mService,
 		_lService,
 		coreService,
-		_uService
+		_uService,
+		_dcService,
+		_lecService
 	},
 	helpers: {}
 };
