454cd40840ee7193a3a2b5bb5eb81ca6adc38f93
Otis Chiemezie <otisidevelop@gmail.com>
2020-01-29 19:28:21 +0100

    Added Student module

---
+++ src/modules/student/resolvers.js
@@ -0,0 +1,207 @@
+const { AuthenticationError, ApolloError } = require("apollo-server");
+
+const resolvers = {
+	Query: {
+		GetStudentsByDepartment: async (_, __, { dataSources, user }) => {
+			if (user) {
+				const { department: d, page, limit } = __;
+				return await dataSources._studService.GetStudentsByDepartment(
+					d,
+					page,
+					limit
+				);
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		},
+		GetStudents: async (_, __, { dataSources, user }) => {
+			if (user) {
+				const { page, limit } = __;
+				return await dataSources._studService.GetStudents(page, limit);
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		},
+		GetStudentById: async (_, { id }, { dataSources, user }) => {
+			if (user) {
+				return await dataSources._studService.GetStudentByid(id);
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		},
+		GetStudentByNo: async (_, { id }, { dataSources, user }) => {
+			if (user) {
+				return await dataSources._studService.GetStudentByNo(id);
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		},
+		GetCoursemate: async (_, __, { dataSources, user }) => {
+			if (user) {
+				const { _studService, _sService } = dataSources;
+				// get student
+				const student_res = await _studService.GetStudentByid(user.id);
+				// get current session
+				const session_res = await _sService.GetActiveSession();
+				// return list of departmental course id
+				const departalCourseId = student_res.doc.registeredCourses.map(
+					i => i.departmentalCourse
+				);
+				const result = await _studService.GetStudentsOfSameClass(
+					departalCourseId,
+					session_res.doc.id
+				);
+				return result;
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		},
+		GetRegisteredCourses: async (_, __, { dataSources, user }) => {
+			if (user) {
+				//
+				const { _sService, _studService } = dataSources;
+				// session
+				const session_res = await _sService.GetActiveSession();
+				const result = await _studService.GetStudentRegisteredCourseList(
+					user.id,
+					session_res.doc.id
+				);
+				return result;
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		}
+	},
+	Mutation: {
+		CreateStudent: async (_, { model }, { dataSources }) => {
+			const { _studService, coreService } = dataSources;
+			// encrypt password
+			const passphase = await coreService.EncryptPassword(model.password);
+			// create student account
+			const result = await _studService.NewStudent(model, passphase);
+			// create token
+			const { id, name, reg_no, email, phone } = result.doc;
+			const token = coreService.GenerateToken({
+				id,
+				name,
+				reg_no,
+				email,
+				phone
+			});
+			// TODO: send email notification
+
+			// return result
+			return {
+				...result,
+				token
+			};
+		},
+		UpdateStudent: async (_, __, { dataSources, user }) => {
+			if (user) {
+				const { name, phone } = __;
+				return await dataSources._studService.UpdateStudent(
+					user.id,
+					name,
+					phone
+				);
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		},
+		UpdateStudentBiometric: async (_, __, { dataSources, user }) => {
+			if (user) {
+				const { id, fingerprint: finger } = __;
+				return await dataSources._studService.UpdateFingerprint(
+					id,
+					finger
+				);
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		},
+		DeleteStudent: async (_, { id }, { dataSources, user }) => {
+			if (user) {
+				return await dataSources._studService.RemoveStudent(id);
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		},
+		StudentLogin: async (_, { no, password }, { dataSources }) => {
+			const { _studService, coreService } = dataSources;
+			// get student by email/phone/reg no
+			const student_res = await _studService.GetStudentByNo(no);
+			// match the password
+			const match = await coreService.ComparePasswords(
+				password,
+				student_res.doc.passphase
+			);
+			// password validation
+			if (!match) return new ApolloError("Incorrect credentials!");
+			// create token
+			const { id, name, reg_no, email, phone } = student_res.doc;
+			const token = coreService.GenerateToken({
+				id,
+				name,
+				reg_no,
+				email,
+				phone
+			});
+			// return
+			return {
+				...student_res,
+				message: "Authenticated successfully!",
+				token
+			};
+		},
+		UpdateStudentLevel: async (_, { level }, { dataSources, user }) => {
+			if (user) {
+				return await dataSources._studService.UpdateLevel(
+					user.id,
+					level
+				);
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		},
+		UpdateStudentImage: async (
+			_,
+			{ image_path },
+			{ dataSources, user }
+		) => {
+			if (user) {
+				return await dataSources._studService.UpdateStudentImage(
+					user.id,
+					image_path
+				);
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		},
+		StudentCourseRegistration: async (_, __, { dataSources, user }) => {
+			if (user) {
+				const { departmentalCourse: courses } = __;
+				const { _studService, _sService } = dataSources;
+				// get student
+				const student_res = await _studService.GetStudentByid(user.id);
+				// get active session
+				const session_res = await _sService.GetActiveSession();
+				const { level } = student_res.doc;
+				// Course registration
+				const result = await _studService.UpdateRegisteredCourse(
+					user.id,
+					courses,
+					session_res.doc.id,
+					level
+				);
+				return result;
+			}
+			return new AuthenticationError("Unauthorized Access!");
+		}
+	},
+	Student: {
+		created_at: ({ created_at }) => new Date(created_at).toISOString(),
+		assigned_courses: ({ assigned_courses }) => {
+			if (
+				assigned_courses.length &&
+				assigned_courses.some(c => typeof c !== "object")
+			)
+				return [];
+			return assigned_courses;
+		},
+		department: ({ department }) => {
+			if (typeof department !== "object") return null;
+			return department;
+		}
+	}
+};
+
+exports.resolvers = resolvers;
---
+++ src/modules/student/typeDefs.js
@@ -0,0 +1,197 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+	extend type Query {
+		"List of Student in a particular department"
+		GetStudentsByDepartment(
+			"Department id"
+			departmental: ID!
+			"Page no"
+			page: Int
+			"Maximum record per request"
+			limit: Int
+		): StudentListResponse!
+		"List of Student in a the entire campus"
+		GetStudents(
+			"Page no"
+			page: Int
+			"Maximum record per request"
+			limit: Int
+		): StudentListResponse!
+		"Gets a single Student using its' id"
+		GetStudentById("Student id" id: ID!): StudentResponse!
+		GetStudentByNo(
+			"Student phone/email/reg no"
+			id: String!
+		): StudentResponse!
+
+		"Gets a list of student that registered the same course."
+		GetCoursemate: StudentListResponse!
+		"Gets a list of course registered by student (current session)"
+		GetRegisteredCourses: StudentRegisteredCourseResponse!
+	}
+
+	extend type Mutation {
+		"Creates new Student object"
+		CreateStudent(
+			"Student model"
+			model: StudentInput!
+		): StudentLoginResponse!
+
+		"Update a single Student object"
+		UpdateStudent(
+			"New Student name"
+			name: String!
+			"Phone number"
+			phone: String!
+		): StudentResponse!
+		"Removes a single Student object"
+		DeleteStudent("Student id" id: ID!): DeletedResponse!
+		"Update a single Student's fingerprint data"
+		UpdateStudentBiometric(
+			"Student's id"
+			id: ID!
+			"Fingerprint template"
+			fingerprint: ID!
+		): StudentResponse!
+
+		"Updates student current level"
+		UpdateStudentLevel("Student new level" level: Int!): StudentResponse!
+		"Updates student current level"
+		UpdateStudentImage(
+			"Student image path"
+			image_path: String!
+		): StudentResponse!
+
+		StudentCourseRegistration(
+			"List of departmental course id"
+			departmentalCourse: [ID!]!
+		): StudentResponse!
+
+		"Student login"
+		StudentLogin(
+			"Student email/phone/reg no"
+			no: String!
+			"Password"
+			password: String!
+		): StudentLoginResponse!
+	}
+	"Student input template"
+	input StudentInput {
+		"Student's name"
+		name: String!
+		"Student's email address"
+		email: String!
+		"Student's phone number"
+		phone: String!
+		"Student reg no"
+		regNo: String!
+		"Password"
+		password: String!
+		"Student's level"
+		level: Int!
+		"Student's department's id"
+		department: ID!
+		"Student passport path"
+		image: String
+	}
+
+	"Student list response template"
+	type StudentListResponse {
+		"Request status code"
+		status: Int!
+		"Request status message"
+		message: String!
+		"Request data"
+		docs: [Student!]
+		"Page number"
+		page: Int
+		"Maximum record per request"
+		limit: Int
+		"Total number of document"
+		totalDocs: Int
+		"Total pages"
+		totalPages: Int
+		"Next page number"
+		nextPage: Int
+		"Previous page number"
+		prevPage: Int
+	}
+	"Single Student response template"
+	type StudentResponse {
+		"Request status code"
+		status: Int!
+		"Request status message"
+		message: String!
+		"Request data"
+		doc: Student!
+	}
+	"Single Student Login response template"
+	type StudentLoginResponse {
+		"Request status code"
+		status: Int!
+		"Request status message"
+		message: String!
+		"Request data"
+		doc: Student!
+		"Student login token"
+		token: String!
+	}
+
+	"Student object template"
+	type Student {
+		"Student id"
+		id: ID!
+		"Student's name"
+		name: String!
+		"Phone number"
+		phone: String!
+		"email address"
+		email: String!
+		"Fingerprint template (Binary)"
+		fingerprint: ID
+		"Date created"
+		created_at: String!
+		"Assigned departmental course"
+		assigned_courses: [DepartmentalCourse!]
+		"Student password"
+		image: String
+		"Reg no"
+		reg_no: String!
+		"Department"
+		department: Department
+	}
+
+	"Student course response template"
+	type StudentRegisteredCourseResponse {
+		"Response status code"
+		status: Int!
+		"Response status message"
+		message: String!
+		"Response  data"
+		docs: [StudentRegisteredCourseModel!]
+	}
+
+	"Stundent Registered course list template"
+	type StudentRegisteredCourseModel {
+		"School session and semester"
+		session: Session!
+		"Student level"
+		level: Int!
+		"List of student course within a group"
+		courses: [StudentCourseModel!]
+		"Total number of student course found with the group"
+		total: Int!
+	}
+	"Student course model template"
+	type StudentCourseModel {
+		"Registered course id"
+		id: ID
+		"Date the course was registered"
+		date: String!
+		"Department course object"
+		course: DepartmentalCourse!
+	}
+`;
+
+exports.typeDefs = typeDefs;
